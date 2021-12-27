c2fdf50a5be4dd1b763ddccba25fa0b530ce0228
Roberto Galaz <roberto.galaz@trafkin.com>
2021-12-27 17:41:37 -0300

    network config

--- __main__.py
+++ __main__.py
@@ -6,14 +6,19 @@ from eb_env_settings import EbEnvSetting
 import pulumi_aws as aws
 
 import json
+import os
 
 with open("salts.json","r") as f:
     salts_dict = json.load(f)
 
-eb_name = "wp-eb-rob"
-vpc_id = "vpc-00fcd97ae96cce58c"
-sg_id = "sg-07716f1761a895b86"
-createRole("wp-eb-rob")
+eb_name = os.environ.get("EB_NAME")
+vpc_id = os.environ.get("EB_VPC_ID")
+sg_id = os.environ.get("EB_RDS_SG_ID")
+env_subnets = "subnet-02e5d5ec1f202811e, subnet-081e7f228051433b2" #privates
+lb_subnets = "subnet-014730c82b5781e84, subnet-0618225b91041e6ee" #publics
+
+createRole(eb_name)
 class_db = DataBaseBuilder("wpeb", "db.t3.micro", "wordpress", "wordpress")
-class_list_settings = EbEnvSetting(vpc_id, class_db.eb_db, salts_dict, sg_id, eb_name)
+class_list_settings = EbEnvSetting(vpc_id, class_db.eb_db, salts_dict, sg_id, eb_name,
+    env_subnets, lb_subnets)
 eb = ElasticBeanstalkBuilder(eb_name, class_list_settings.list_settings)
---
+++ declarate-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+export EB_VPC_ID="vpc-019a06f040ec5fa0d"
+export EB_RDS_SG_ID="sg-0c60f12d7417d2f24"
+export EB_NAME="wp-eb-rob"
+echo "variables declared!"
