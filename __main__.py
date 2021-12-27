168580a330075a8547f383afcb1ebf22a4a5ac81
Mana <umbrannarcissus@hotmail.com>
2021-12-27 16:21:37 -0300

    Subnet string retrieve func

--- __main__.py
+++ __main__.py
@@ -11,24 +11,27 @@ with open("salts.json","r") as f:
     salts_dict = json.load(f)
 
 eb_name = "wp-eb-rob"
-vpc_id = "vpc-00fcd97ae96cce58c"
+vpc_id = "vpc-0a740f406904bcc81"
 sg_id = "sg-07716f1761a895b86"
 
 
-VPCDefinition = collections.namedtuple('VPCDefinition', ['vpc_id', 'public_subnet_ids', 'private_subnet_ids'])
+private_subnet = []
+public_subnet = []
 
-def retrieve_vpc_and_subnets(vpc) -> VPCDefinition:
-    _public_subnet_ids = aws.ec2.get_subnet_ids(vpc_id=vpc_id,
+def public_subnet_retrieve(_vpc_ids: str) -> public_subnet:
+    _public_subnet_ids = aws.ec2.get_subnet_ids(vpc_id=_vpc_ids,
                                                 tags={"Type": "Public"}).ids
-    _private_subnet_ids = aws.ec2.get_subnet_ids(vpc_id=vpc_id,
+
+def private_subnet_retrieve(_vpc_ids: str) -> public_subnet:
+        _private_subnet_ids = aws.ec2.get_subnet_ids(vpc_id=_vpc_ids,
                                                 tags={"Type": "Private"}).ids
-    return VPCDefinition(vpc_id=vpc_id, public_subnet_ids=_public_subnet_ids, private_subnet_ids=_private_subnet_ids)
 
+private_subnet = private_subnet_retrieve(vpc_id)
+public_subnet = public_subnet_retrieve(vpc_id)
 
-Private_subnet = VPCDefinition['private_subnet_ids']
-Public_subnet = VPCDefinition['public_subnet_ids']
+print(public_subnet)
 
 createRole("wp-eb-rob")
 class_db = DataBaseBuilder("wpeb", "db.t3.micro", "wordpress", "wordpress")
 class_list_settings = EbEnvSetting(vpc_id, class_db.eb_db, salts_dict, sg_id, eb_name)
-eb = ElasticBeanstalkBuilder(eb_name, class_list_settings.list_settings,private_subnet_ids=Private_subnet,public_subnet_ids=Public_subnet)
+eb = ElasticBeanstalkBuilder(eb_name, class_list_settings.list_settings)
